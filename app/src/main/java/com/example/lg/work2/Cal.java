38140e165017572d35d63a1e06ce9dc260352c3a
totoey200 <totoey200@gmail.com>
2017-03-14 18:19:25 +0900

    work2 second commit

--- app/src/main/java/com/example/lg/work2/Cal.java
+++ app/src/main/java/com/example/lg/work2/Cal.java
@@ -2,12 +2,120 @@ package com.example.lg.work2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Cal extends AppCompatActivity {
 
+    EditText num1,num2;
+    Button plus_btn,minus_btn,multi_btn,divd_btn;
+    String input1;
+    String input2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cal);
+        setTitle("계산기");
+        init();
+        plus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input1 = num1.getText().toString();
+                input2 = num2.getText().toString();
+                if (input1.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num1.requestFocus();
+                }
+                else if(input2.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num2.requestFocus();
+                }
+                else{
+                    int result = Integer.parseInt(input1)+Integer.parseInt(input2);
+                    Toast.makeText(getApplicationContext(),"더하기 계산 결과는 "+result+"입니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        minus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input1 = num1.getText().toString();
+                input2 = num2.getText().toString();
+                if (input1.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num1.requestFocus();
+                }
+                else if(input2.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num2.requestFocus();
+                }
+                else{
+                    int result = Integer.parseInt(input1)-Integer.parseInt(input2);
+                    Toast.makeText(getApplicationContext(),"빼기 계산 결과는 "+result+"입니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        multi_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input1 = num1.getText().toString();
+                input2 = num2.getText().toString();
+                if (input1.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num1.requestFocus();
+                }
+                else if(input2.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num2.requestFocus();
+                }
+                else{
+                    int result = Integer.parseInt(input1)*Integer.parseInt(input2);
+                    Toast.makeText(getApplicationContext(),"곱하기 계산 결과는 "+result+"입니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        divd_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input1 = num1.getText().toString();
+                input2 = num2.getText().toString();
+                if (input1.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num1.requestFocus();
+                }
+                else if(input2.equals("")){
+                    Toast.makeText(getApplicationContext(),"값을 입력하세요",
+                            Toast.LENGTH_SHORT).show();
+                    num2.requestFocus();
+                }
+                else{
+                    double p = Double.parseDouble(input1)/Double.parseDouble(input2);
+                    int result = (int) p;
+                    Toast.makeText(getApplicationContext(),"나누기 계산 결과는 "+result+"입니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    void init(){
+
+        num1 = (EditText)findViewById(R.id.input_num1);
+        num2 = (EditText)findViewById(R.id.input_num2);
+        plus_btn = (Button)findViewById(R.id.plus);
+        minus_btn = (Button)findViewById(R.id.minus);
+        multi_btn = (Button)findViewById(R.id.multi);
+        divd_btn = (Button)findViewById(R.id.divide);
     }
 }
--- app/src/main/java/com/example/lg/work2/Order.java
+++ app/src/main/java/com/example/lg/work2/Order.java
@@ -2,6 +2,8 @@ package com.example.lg.work2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 public class Order extends AppCompatActivity {
     EditText pi_cnt,spa_cnt,sal_cnt;
     CheckBox cb;
+    Button cal;
     TextView tv_cnt,tv_price;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +20,52 @@ public class Order extends AppCompatActivity {
 
         setTitle("레스토랑 메뉴 주문");
 
+        init();
 
+        cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pizza = pi_cnt.getText().toString();
+                String spa = spa_cnt.getText().toString();
+                String sal = sal_cnt.getText().toString();
+                int result=0;
+                int cnt = 0;
+                if(pizza.equals("")){
+                    pi_cnt.setText("0");
+                }
+                else{
+                    result += Integer.parseInt(pizza)*15000;
+                    cnt += Integer.parseInt(pizza);
+                }
+                if(spa.equals("")){
+                    spa_cnt.setText("0");
+                }
+                else{
+                    result += Integer.parseInt(spa)*13000;
+                    cnt += Integer.parseInt(spa);
+                }
+                if(sal.equals("")){
+                    sal_cnt.setText("0");
+                }
+                else{
+                    result += Integer.parseInt(sal)*9000;
+                    cnt += Integer.parseInt(sal);
+                }
+                if(cb.isChecked()){
+                    result *= 0.9;
+                }
+                tv_cnt.setText(Integer.toString(cnt));
+                tv_price.setText(Integer.toString(result));
+            }
+        });
     }
     void init(){
         pi_cnt = (EditText)findViewById(R.id.input_pizza);
         spa_cnt = (EditText)findViewById(R.id.input_spa);
         sal_cnt = (EditText)findViewById(R.id.input_sal);
-        cb = (CheckBox)findViewById(R.id.)
+        cb = (CheckBox)findViewById(R.id.cb);
+        cal = (Button)findViewById(R.id.order_cal);
+        tv_cnt = (TextView)findViewById(R.id.result_order_count);
+        tv_price = (TextView)findViewById(R.id.result_order_price);
     }
 }
